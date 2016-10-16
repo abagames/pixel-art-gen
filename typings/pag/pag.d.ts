636be19abdadc3f066f0faea212bfea76fede081
ABA Games <cs8k-cyu@asahi-net.or.jp>
2016-10-16 09:12:28 +0900

    Declare PagOptions in the module

--- typings/pag/pag.d.ts
+++ typings/pag/pag.d.ts
@@ -9,22 +9,21 @@ declare module 'pag' {
     isEmpty: boolean;
     style: string;
   }
-}
-
-declare interface PagOptions {
-  isMirrorX?: boolean;
-  isMirrorY?: boolean;
-  seed?: number;
-  hue?: number;
-  saturation?: number,
-  value?: number,
-  rotationNum?: number,
-  scale?: number,
-  scaleX?: number,
-  scaleY?: number,
-  colorNoise?: number,
-  colorLighting?: number,
-  edgeDarkness?: number,
-  isShowingEdge?: boolean,
-  isShowingBody?: boolean,
+  interface PagOptions {
+    isMirrorX?: boolean;
+    isMirrorY?: boolean;
+    seed?: number;
+    hue?: number;
+    saturation?: number,
+    value?: number,
+    rotationNum?: number,
+    scale?: number,
+    scaleX?: number,
+    scaleY?: number,
+    colorNoise?: number,
+    colorLighting?: number,
+    edgeDarkness?: number,
+    isShowingEdge?: boolean,
+    isShowingBody?: boolean,
+  }
 }
